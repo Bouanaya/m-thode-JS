ccdbde0a624213cf37c53f1907ed21c3daf755a5
Bouanaya <bouanayasoufiane@gmail.com>
2024-11-03 10:18:18 +0100

    add find Index

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // FindLast//
 const arr = [ 12 , 23 , 100, 122, 13 , 90]
-// findLast new array  callback
+findIndex
 const newArray = arr.findLast(x=>x > 100)
 // return last number > 100
 console.log(newArray);
+
+
+
+// findIndex//
+// return last number > 100
+const arr1 = arr.findIndex(x=>x==23)
+// return index 23  this index = 1
+console.log(arr1);
+
