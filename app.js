7e04f117e9a44ab3a89ed4156eec012fce8f8ffe
Bouanaya <bouanayasoufiane@gmail.com>
2024-11-03 10:05:04 +0100

    add  methode findLast

---
+++ app.js
@@ -0,0 +1,6 @@
+// FindLast//
+const arr = [ 12 , 23 , 100, 122, 13 , 90]
+// findLast new array  callback
+const newArray = arr.findLast(x=>x > 100)
+// return last number > 100
+console.log(newArray);
